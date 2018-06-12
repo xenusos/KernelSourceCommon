1444a3ab471c21aae38aef1d1385112f024c9310
Reece Wilson <reece@itsghost.me>
2018-06-12 19:23:12 +0100

    KERNEL 2dd5b90bf0ea58764aed12abe8f20e107a8a30f7

--- headers/plugins/plugins.h
+++ headers/plugins/plugins.h
@@ -1,3 +1,3 @@
 #pragma once
-XENUS_SYM error_t xenus_load_module(char * friendly_name, char * mod, char * path, size_t * unresolved_symbols, void ** handle);
+XENUS_SYM error_t xenus_load_module(char * friendly_name, char * mod, char * path, void ** handle);
 XENUS_SYM error_t xenus_load_plugin(char * name, char * mod, char * path);
