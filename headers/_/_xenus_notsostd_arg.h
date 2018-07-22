5d304588508677007a753bbe1daa14e939270b51
Reece Wilson <reece@itsghost.me>
2018-07-22 11:32:44 +0100

    Update notsostdarg

--- headers/_/_xenus_notsostd_arg.h
+++ headers/_/_xenus_notsostd_arg.h
@@ -1,3 +1,9 @@
+/*
+    Purpose: Unfuck stdarg.h
+    Author : Microsoft Corporation, Reece Wilson
+    License: NaN - Public Domain
+			 Insufficient work to warrent protection; arbitrary strings and symbols do not warrent protection under respective laws due to the inherently public nature of the primative abstract concepts at play and simplicity
+*/
 #pragma once
 #pragma pack(push,8)
 
@@ -6,7 +12,7 @@ void __cdecl __va_start(va_list*, ...);
 
 #define __crt_va_start_a(ap, x) ((void)(__va_start(&ap, x)))
 
-#if defined __cplusplus 
+#if defined(__cplusplus)
 	extern "C++"
 	{
 		template <typename _Ty>
@@ -38,10 +44,10 @@ void __cdecl __va_start(va_list*, ...);
 	#define va_start(ap, x) __crt_va_start_a(ap, x)
 #endif
 
-#define va_arg(ap, t)   \
-    ( ( sizeof(t) > sizeof(__int64) || ( sizeof(t) & (sizeof(t) - 1) ) != 0 ) \
-        ? **(t **)( ( ap += sizeof(__int64) ) - sizeof(__int64) ) \
-        :  *(t  *)( ( ap += sizeof(__int64) ) - sizeof(__int64) ) )
+#define va_arg(ap, t)																					\
+								((sizeof(t) > sizeof(__int64) || (sizeof(t) & (sizeof(t) - 1) ) != 0)	\
+							    ? **(t **)((ap += sizeof(__int64)) - sizeof(__int64))					\
+							    :  *(t  *)((ap += sizeof(__int64)) - sizeof(__int64)))
 #define va_end(ap)      ( ap = (va_list)0 )
 
 #pragma pack(pop)
