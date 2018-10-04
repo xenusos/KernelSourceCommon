59c2e3c4f507dc46077f63fbe0a656d5899cb355
Reece W <me@reece.sx>
2018-10-04 22:06:27 +0100

    Add additional kernel symbols related to memory management

--- headers/kernel/libx/xenus_memory.h
+++ headers/kernel/libx/xenus_memory.h
@@ -6,10 +6,19 @@
 */
 #pragma once
 
+XENUS_SYM size_t kernel_memusage;
+
+XENUS_SYM void alloc_increment_atomicity();
+XENUS_SYM void alloc_decrement_atomicity();
+XENUS_SYM uint32_t alloc_get_atomicity();
+
+XENUS_SYM void * malloc_atomic(size_t length);
 
 XENUS_SYM void * malloc(size_t length);
+XENUS_SYM void * zalloc(size_t length);
 XENUS_SYM void free(void * s);
 XENUS_SYM void * calloc(size_t num, size_t size);
+XENUS_SYM void * calloc_nozero(size_t num, size_t size);
 XENUS_SYM void * realloc(void * buf, size_t length);
 
 XENUS_SYM size_t alloc_actual_size(void * s);
