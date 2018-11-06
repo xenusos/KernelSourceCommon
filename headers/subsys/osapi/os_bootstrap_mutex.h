b220cfb3fb7b0dde8cb52f2dbc61ccb6a69bf094
Reece W <me@reece.sx>
2018-11-06 09:54:32 +0000

    Typo

--- headers/subsys/osapi/os_bootstrap_mutex.h
+++ headers/subsys/osapi/os_bootstrap_mutex.h
@@ -1,7 +1,7 @@
-XENUS_IMPORT void *      mutex_init();
+XENUS_IMPORT mutex_k     mutex_init();
 XENUS_IMPORT void        mutex_lock(mutex_k mutex);
 XENUS_IMPORT int         mutex_trylock(mutex_k mutex);
 XENUS_IMPORT void        mutex_unlock(mutex_k mutex);
-XENUS_IMPORT void        mutex_destory(mutex_k mutex);
+XENUS_IMPORT void        mutex_destroy(mutex_k mutex);
 XENUS_IMPORT int         mutex_is_locked(mutex_k mutex);
 #define mutex_create mutex_init
